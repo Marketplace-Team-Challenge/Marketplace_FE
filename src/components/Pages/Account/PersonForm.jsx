29ae5dff5686b153cb96374710ada2ba9da55fe2
Taras Papka <taras.papka@protonmail.com>
2023-07-31 13:31:03 +0300

    addaptive form

--- src/components/Pages/Account/PersonForm.jsx
+++ src/components/Pages/Account/PersonForm.jsx
@@ -1,18 +1,69 @@
 import { ButtonMain } from "../../UI/Button/ButtonMain"
 import css from "./PersonForm.module.css"
+
+const formData = [
+  {
+    label: "First name",
+    name: "name",
+    type: "text",
+    placeholder: "Enter your First name"
+  },
+  {
+    label: "Country/region fo residence",
+    name: "residence",
+    type: "text",
+    placeholder: "Enter your country of residence"
+  },
+  {
+    label: "Country/region code",
+    name: "code",
+    type: "text",
+    placeholder: "Enter your region code"
+  },
+  {
+    label: "Date of birth",
+    name: "birthday",
+    type: "date",
+    placeholder: "Enter your birthday"
+  },
+{
+    label: "Last name",
+    name: "surname",
+    type: "text",
+    placeholder: "Enter your Last name"
+  },
+  {
+    label: "Last name",
+    name: "surname",
+    type: "text",
+    placeholder: "Enter your Last name"
+  },
+  {
+    label: "Last name",
+    name: "surname",
+    type: "text",
+    placeholder: "Enter your Last name"
+  },
+  {
+    label: "Last name",
+    name: "surname",
+    type: "text",
+    placeholder: "Enter your Last name"
+  }
+]
 export const Form = () => {
     return (
-         <form className={css.form}>
-        <div className={css.form_group}>
-          <label htmlFor="name">First name</label>
-          <input required="" placeholder="Enter your name" name="name" id="name" type="text"/>
+      <form className={css.form}>
+        <div className={css.form_wrapper}>
+          {formData.map((data) => {
+                    return(      
+                        <div className={css.form_group} key={data.index}>
+                        <label htmlFor={data.name}>{ data.label}</label>
+                        <input required="" placeholder={data.placeholder} name={data.name} id={data.name} type={ data.type} />
         </div>
-
-        <div className={css.form_group}>
-          <label htmlFor="password">Password</label>
-          <input required="" name="password" placeholder="Enter your password" id="password" type="password"/>
+                    )
+                })}
         </div>
-
         <ButtonMain/>
       </form>
     )
--- src/components/Pages/Account/PersonalInfo.jsx
+++ src/components/Pages/Account/PersonalInfo.jsx
@@ -8,8 +8,8 @@ export const PersonalInfo = () => {
             <div className={css.personal_wrapper}>
                 <h3 className={css.personal_title}>Personal Info</h3>
             <p className={css.personal_info}>This information is used to autofill your details. Your details are stored securely and will not be shared publicly.</p>
-            <Form/>
             </div>
+            <Form/>
             
         </div>
     )
