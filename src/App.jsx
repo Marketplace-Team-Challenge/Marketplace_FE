94ceaa1669034110c08b848810af03de727b81ea
Taras Papka <taras.papka@protonmail.com>
2023-08-02 22:43:02 +0300

    clear all data to start project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-import { CardComponent } from './components/UI/CardComponent/CardComponent'
-import { SearchComponent } from './components/UI/SearchComponent/Search'
-import { SpecialOffers } from './components/UI/SpecialOffers/SpecialOffers'
-
 function App() {
 
 
   return (
     <>      
-      <SearchComponent />
-      <CardComponent />
-      <SpecialOffers/>
+<h1>Main page</h1>
     </>
   )
 }
