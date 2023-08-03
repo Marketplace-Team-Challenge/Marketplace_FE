ee11ace9902fbfd97b5d5437d3fc61dc946d8fee
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-08-03 09:46:46 +0300

    delete SearchComponent, CardComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
 import { Route, Routes } from 'react-router-dom'
 // import { FooterComponent } from './components/UI/FooterComponent/FooterComponent'
 import { HeaderComponent } from './components/UI/HeaderComponent/HeaderComponent'
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,7 @@
-import { CardComponent } from "../components/UI/CardComponent/CardComponent";
-import { SearchComponent } from "../components/UI/SearchComponent/Search";
-import { SpecialOffers } from "../components/UI/SpecialOffers/SpecialOffers";
-
 export const MainPage = () => {
     return (
         <>
-        <SearchComponent />
-        <CardComponent />
-        <SpecialOffers />
+       <h1>Main</h1>
         </>
     );
 }
