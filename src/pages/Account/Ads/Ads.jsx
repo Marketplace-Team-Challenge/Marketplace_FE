4107013fee6aa2cbf6b30ef2649a79cef3bb967f
Taras Papka <taras.papka@protonmail.com>
2023-08-16 23:34:55 +0300

    changed style, pass to img and style

--- src/pages/Account/Ads/Ads.jsx
+++ src/pages/Account/Ads/Ads.jsx
@@ -1,11 +1,11 @@
-// import { ButtonMain } from '../../../components/UI/common/ButtonComponent/ButtonMain'
+import { ButtonMain } from '../../../components/UI/common/ButtonComponent/ButtonMain'
 import css from './Add.module.css'
 import { NoAds } from './NoAds'
 export const Ads = () => {
     return (
         <div className={css.personal_container}>
         <NoAds /> 
-        
+        <ButtonMain className={css.ads_btn} type="submit" buttonName="Create an ad"/>
         </div>
         
     )
